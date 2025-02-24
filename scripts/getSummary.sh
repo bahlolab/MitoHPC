ba1b8526543e399d00ecf887839e5f4922ab4342
Longfei Wang <54878372+longfei-stat@users.noreply.github.com>
2025-02-24 19:08:06 +1100

    Update mtDNA-CN

--- scripts/getSummary.sh
+++ scripts/getSummary.sh
@@ -12,7 +12,7 @@ if  [ "$#" -lt 1 ] ; then ODIR=$HP_ODIR ; else ODIR=$1 ; fi
 
 #get read count and mtDN-CN stats 
 if [ $HP_CN ]  && [ $HP_CN -ne 0 ] || [ ! -s $ODIR/count.tab ] ; then
-    cut -f3 $HP_IN | perl -ane 'print "$F[0].count\n";' | xargs cat | cut -f1,2,3,4 | uniq.pl | getCN.pl > $ODIR/count.tab
+    cut -f3 $HP_IN | sed '1d' | perl -ane 'print "$F[0].count\n";' | xargs cat | cut -f1,2,3,4 | uniq.pl | getCN.pl > $ODIR/count.tab
 fi
 if [ $HP_I -lt 1 ] ; then exit 0 ; fi
 
